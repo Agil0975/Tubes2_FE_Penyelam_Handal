c4ab48fbbe38f9b000ad876153648de1a923aa51
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-04-25 21:33:36 +0700

    fix: CORS fix

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -50,9 +50,9 @@ export default function InputForm() {
 
     fetch(url)
       .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+        // if (!response.found) {
+        //   throw new Error("Network response was not ok");
+        // }
         return response.json();
       })
       .then((data) => {
